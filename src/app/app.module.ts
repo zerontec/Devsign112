575e31805c3399102aa9e92695f70205b12ea213
unknown <zerontecnologia1@gmail.com>
2018-10-09 12:25:21 -0400

    creacion componentes estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NoFoundPagesComponent } from './pages/no-found-pages/no-found-pages.component';
+import { NavbarComponent } from './pages/navbar/navbar.component';
+import { FooterComponent } from './pages/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServicesComponent,
+    ContactComponent,
+    RegisterComponent,
+    LoginComponent,
+    NoFoundPagesComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
